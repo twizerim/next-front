2e4d009c1bca405e31cfb9c7720d9b109fdd86ba
ATLP <atlp@ATLPs-MBP.lan>
2024-08-05 09:28:19 +0200

    navbar

---
+++ src/app/component/share/layout/navbar.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Navbar(){
+    return (
+       <nav className="bg-red-500">
+         <div>
+            <h1>Design</h1>
+         </div>
+         <ul>
+            <li><a href="">Sign up</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Calendal</a></li>
+            <li><a href="">Calculater</a></li>
+            
+         </ul>
+       </nav>
+    )
+}
